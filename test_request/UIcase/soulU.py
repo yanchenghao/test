5867be03be5c027c6af0ac4ab296ef65544116cf
yanchenghao <1147889259@qq.com>
2022-11-17 21:57:26 +0800

    封装login

--- test_request/UIcase/soulU.py
+++ test_request/UIcase/soulU.py
@@ -10,6 +10,42 @@ sys.path.append("/Users/yanchenghao/PycharmProjects/PycharmProjects/pythonProjec
 
 from common import UIapi_config
 import time
+def login(driver,phone,password):
+
+  el2 = driver.find_element(AppiumBy.XPATH, "//*[@text='Phone']")
+
+  try:
+    if el2:
+      # print(driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR, value="phone"))
+      el2.click()
+      time.sleep(3)
+      el3=driver.find_element(AppiumBy.ID,"com.haflla.soulu:id/mobile")
+      el3.send_keys(phone)
+      driver.keyevent(4)
+      print(f"{i}")
+      time.sleep(1)
+      driver.find_element(AppiumBy.ID,"com.haflla.soulu:id/text").click()
+      time.sleep(5)
+      size=driver.get_window_size()
+      print(size)
+      touchobject=TouchAction(driver)
+      touchobject.tap(x=200, y=200).release().perform()
+      time.sleep(2)
+      el4=driver.find_element(AppiumBy.ID,"com.haflla.soulu:id/password")
+      time.sleep(3)
+      el4.send_keys("password")
+      driver.hide_keyboard()
+      time.sleep(2)
+      el4 = driver.find_element(AppiumBy.ID, "com.haflla.soulu:id/btn_next")
+      el4.click()
+
+    else:
+      print("sdsfsdfs")
+
+  except:
+    print(traceback.format_exc())
+  finally:
+    time.sleep(8)
 
 from appium.webdriver.common.appiumby import AppiumBy
 from selenium.webdriver.common.by import By
@@ -28,38 +64,5 @@ for i in range(3000001,3000002):
   # }
   # driver = webdriver.Remote(server,desired_caps)
   time.sleep(4)
-  el2 = driver.find_element(AppiumBy.XPATH, "//*[@text='Phone']")
-
-  try:
-    if el2:
-      # print(driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR, value="phone"))
-      el2.click()
-      time.sleep(3)
-      el3=driver.find_element(AppiumBy.ID,"com.haflla.soulu:id/mobile")
-      el3.send_keys(f"{i}")
-      driver.keyevent(4)
-      print(f"{i}")
-      time.sleep(1)
-      driver.find_element(AppiumBy.ID,"com.haflla.soulu:id/text").click()
-      time.sleep(5)
-      size=driver.get_window_size()
-      print(size)
-      touchobject=TouchAction(driver)
-      touchobject.tap(x=200, y=200).release().perform()
-      time.sleep(2)
-      el4=driver.find_element(AppiumBy.ID,"com.haflla.soulu:id/password")
-      time.sleep(3)
-      el4.send_keys("Best1234")
-      driver.hide_keyboard()
-      time.sleep(2)
-      el4 = driver.find_element(AppiumBy.ID, "com.haflla.soulu:id/btn_next")
-      el4.click()
-
-    else:
-      print("sdsfsdfs")
-
-  except:
-    print(traceback.format_exc())
-  finally:
-    time.sleep(8)
-    driver.quit()
+  login(driver,i,"Best1234")
+  driver.quit()
